aa3f04b54671228fe349caa7242fed6a478db14b
shivamsrivastava098@gmail.com <shivamsrivastava098@gmail.com>
2024-07-21 20:32:57 +0530

    condition

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hello JavaScipt');
+console.log('Nice');
+
+num = 72653;
+
+myname ='shivam'
+
+console.log(myname);
+
+console.log(typeof num);
+console.log(typeof myname);
+
+console.log(1=='1');
+console.log(1=== '1' );
